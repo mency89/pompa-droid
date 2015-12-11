c2fb0706f02b215e0cdcd17927e88f781c413770
zhangpanyi@live.com <zhangpanyi@live.com>
2015-12-11 11:25:13 +0800

    update

---
+++ Classes/Scene/GameScene.cpp
@@ -0,0 +1,40 @@
+﻿#include "GameScene.h"
+
+#include <Box2D/Box2D.h>
+#include "Entity/Hero.h"
+#include "Entity/BaseGameEntity.h"
+#include "Entity/GameEntityCreator.h"
+using namespace cocos2d;
+
+
+GameScene::GameScene()
+{
+
+}
+
+GameScene::~GameScene()
+{
+
+}
+
+Scene* GameScene::createScene()
+{
+	auto scene = Scene::create();
+	auto layer = GameScene::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool GameScene::init()
+{
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	auto hero = GameEntityCreator<Hero>().create(nullptr);
+	hero->setPosition(Vec2(200, 200));
+	addChild(hero);
+
+	return true;
+}
---
+++ Classes/Scene/WelcomeScene.h
@@ -0,0 +1,16 @@
+﻿#ifndef __WELCOMESCENE_H__
+#define __WELCOMESCENE_H__
+
+#include "cocos2d.h"
+
+class WelcomeScene : public cocos2d::Layer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init() override;
+
+	CREATE_FUNC(WelcomeScene);
+};
+
+#endif
