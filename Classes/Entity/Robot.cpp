7da22e8bd3ed0254d93fbe51092766f8718470f4
zhangpanyi@live.com <zhangpanyi@live.com>
2016-01-11 10:14:54 +0800

    update

--- Classes/Entity/Robot.cpp
+++ Classes/Entity/Robot.cpp
@@ -1,1 +1,64 @@
-﻿
+﻿#include "Robot.h"
+#include "ShapeCategory.h"
+using namespace cocos2d;
+
+
+Robot::Robot(std::shared_ptr<b2World> world)
+	: BaseGameEntity(world)
+	, player_(nullptr)
+{
+
+}
+
+Robot::~Robot()
+{
+
+}
+
+bool Robot::init()
+{
+	if (!BaseGameEntity::init())
+	{
+		return false;
+	}
+
+	state_machine_.reset(new StateMachine<Robot>(this));
+
+	player_ = Sprite::create();
+	player_->setAnchorPoint(Vec2::ZERO);
+	addChild(player_);
+
+	return true;
+}
+
+void Robot::update()
+{
+	state_machine_->update();
+}
+
+const char* Robot::name() const
+{
+	return "Robot";
+}
+
+int Robot::weaponCategoryBits() const
+{
+	return kEnemyWeaponShape;
+}
+
+void Robot::handleMenssage(const Message &msg)
+{
+	// ignore
+}
+
+// 获取特效播放器
+Node* Robot::getEffectsPlayer()
+{
+	return player_;
+}
+
+// 获取有限状态机
+StateMachine<Robot>* Robot::getStateMachine()
+{
+	return state_machine_.get();
+}
--- Classes/Entity/Robot.h
+++ Classes/Entity/Robot.h
@@ -1,1 +1,42 @@
-﻿
+﻿#ifndef __ROBOT_H__
+#define __ROBOT_H__
+
+#include "BaseGameEntity.h"
+#include "State/StateMachine.h"
+
+class Robot : public BaseGameEntity
+{
+	typedef std::auto_ptr<StateMachine<Robot> > RobotStateMachine;
+
+public:
+	Robot(std::shared_ptr<b2World> world);
+
+	~Robot();
+
+	virtual bool init() override;
+
+	virtual void update() override;
+
+	virtual const char* name() const override;
+
+	virtual int weaponCategoryBits() const override;
+
+	virtual void handleMenssage(const Message &msg) override;
+
+public:
+	/**
+	 * 获取特效播放器
+	 */
+	cocos2d::Node* getEffectsPlayer();
+
+	/**
+	 * 获取有限状态机
+	 */
+	StateMachine<Robot>* getStateMachine();
+
+private:
+	cocos2d::Sprite* player_;
+	RobotStateMachine state_machine_;
+};
+
+#endif
